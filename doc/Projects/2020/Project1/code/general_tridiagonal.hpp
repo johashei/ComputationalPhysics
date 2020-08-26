ca48d12341f3b60066ae3d6ef7f2f4a2367fdc0f
johashei <j.s.heines@fys.uio.no>
2020-08-26 08:00:23 +0200

    started work on project 1

---
+++ doc/Projects/2020/Project1/code/general_tridiagonal.hpp
@@ -0,0 +1,13 @@
+#ifndef GENERAL_TRIDIAGONAL_HPP
+#define GENERAL_TRIDIAGONAL_HPP
+
+class general_tridiagonal {
+private:
+  int m_matrix_size;
+  
+
+public:
+
+}
+
+#endif
---
+++ doc/Projects/2020/Project1/code/project1.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <cmath>
+#include "general_tridiagonal.hpp"
+
+using namespace std;
