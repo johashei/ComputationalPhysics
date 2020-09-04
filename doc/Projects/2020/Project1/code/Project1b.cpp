59fba703da6bad545ad21b26cb0503eee7438d22
johashei <j.s.heines@fys.uio.no>
2020-09-04 15:21:25 +0200

    Mostly finished working code, does not need makefile

--- doc/Projects/2020/Project1/code/Project1b.cpp
+++ doc/Projects/2020/Project1/code/Project1b.cpp
@@ -34,44 +34,58 @@ int main(int argc, char *argv[]){
   // Run and time algorithm:
   double* u;
   if(strcmp(argv[3], "general") == 0){
+    cout << "Solving with general algo ..." << flush;
     start = clock(); // start timer
     u = general_algo(n,h);
+    cout << " Done." << endl;
   }
   else if(strcmp(argv[3], "specific") == 0){
+    cout << "Solving with specific algo ..." << flush;
     start = clock(); // start timer
     u = specific_algo(n,h);
-  }
+    cout << " Done." << endl;
+}
   else{
     cout << "Bad usage. <algorithm> should be 'general' or 'specific'." << endl;
     exit(1);
   }
   finish = clock(); // stop timer
+  //cout << finish << " "<< start << " " << CLOCKS_PER_SEC << endl;
   double Time = (double(finish - start)/CLOCKS_PER_SEC);
 
   // Analytic solution
+  cout << "Calculating analytic solution ..." << flush;
   double *v_array = new double[n+2];
   for(int i=0 ; i<n+2 ; i++){
     v_array[i] = v(i*h);
   }
+  cout << " Done." << endl;
 
   // Maximum error
+  cout << "Calculating maximum relative error ..." << flush;
   double maxerror = error(n,u,v_array);
+  cout << " Done." << endl;
 
   // Write results to file
+  //start = clock(); // New clock to time write to file
+  cout << "Writing results to file ..." << flush;
   ofstream ofile;
   ofile.open(outfilename);
-  ofile << "Solution to differential equation with "<< argv[3] << " algorithm:" << endl;
-  ofile << n << endl;
+  ofile << "Solution to differential equation with "<< argv[3] << " algorithm:\n";
+  ofile << n << "\n";
   for(int i=0; i<n+2; i++){
     ofile << setw(15) << setprecision(8) << u[i] << " ";
-    ofile << setw(15) << setprecision(8) << v_array[i] << endl;
+    ofile << setw(15) << setprecision(8) << v_array[i] << "\n";
   }
-  ofile << "Max relative error and log10(h):" << endl;
+  ofile << "Max relative error and log10(h):\n";
   ofile << setw(15) << setprecision(8) << maxerror << " ";
-  ofile << setw(15) << setprecision(8) << log10(h) << endl;
-  ofile << "Time taken by the algorithm [s]:" << endl;
-  ofile << Time << endl;
+  ofile << setw(15) << setprecision(8) << log10(h) << "\n";
+  ofile << "Time taken by the algorithm [s]:\n";
+  ofile << Time << "\n";
   ofile.close();
+  cout << " Done." << endl;
+  //finish=clock();
+  //cout << double(finish - start)/CLOCKS_PER_SEC << endl;
 
   delete [] u; delete [] v_array;
 
@@ -85,8 +99,8 @@ double* general_algo(int n, double h){
   double *c = new double [n-1];
   double *g = new double [n];
   double *u = new double [n+2];
-  double *btilde = new double [n];
-  double *gtilde = new double [n];
+  //double *btilde = new double [n];
+  //double *gtilde = new double [n];
 
   // Initializing:
   for (int i = 0; i<n-1; i++){
@@ -103,21 +117,21 @@ double* general_algo(int n, double h){
   u[0] = 0;
   u[n+1] = 0;
 
-  btilde[0] = b[0];
-  gtilde[0] = g[0];
+  //btilde[0] = b[0];
+  //gtilde[0] = g[0];
 
   // Forward substitution -- 5N FLOPs
   for(int i=1 ; i<n ; i++){
-    double row_reduction_factor = a[i-1]/btilde[i-1]; // used twice so saves one FLOP
-    btilde[i] = b[i] - c[i-1]* row_reduction_factor;
-    gtilde[i] = g[i] - gtilde[i-1]* row_reduction_factor;
-    cout << btilde[i] << " ";
+    double row_reduction_factor = a[i-1]/b[i-1]; // used twice so saves one FLOP
+    b[i] = b[i] - c[i-1]* row_reduction_factor;
+    g[i] = g[i] - g[i-1]* row_reduction_factor;
+    //cout << b[i] << " ";
   }
-  cout << endl;
   //Backward substitution -- 3N FLOPs
   for(int i=n+1 ; i>1 ; i--){
-    u[i-1] = (gtilde[i-2] - c[i-2]*u[i])/btilde[i-2]; // -2 because they start at 0 not 1
+    u[i-1] = (g[i-2] - c[i-2]*u[i])/b[i-2]; // -2 because they start at 0 not 1
   }
+  delete [] a; delete [] b; delete [] c; delete [] g;
   return u;
 }
 
@@ -128,7 +142,7 @@ double* specific_algo(int n, double h){
   double *g = new double [n];
   double *u = new double [n+2];
   double *edlitb = new double [n]; // 1/btilde, because * is faster than /
-  double *gtilde = new double [n];
+  //double *gtilde = new double [n];
 
   // Initializing
   for(int i=0 ; i<n+1; i++){
@@ -138,31 +152,30 @@ double* specific_algo(int n, double h){
   u[0] = 0;
   u[n+1] = 0;
 
-  gtilde[0] = g[0];
+  //gtilde[0] = g[0];
 
   // Precalculating 1/btilde
   edlitb[0] = 1.0/2;
   for(int i=1 ; i<n ; i++){
     edlitb[i] = double(i+1)/(i+2);
-    cout << 1/edlitb[i] << " ";
+    //cout << 1/edlitb[i] << " ";
   }
-  cout << endl;
   // Forward substitution -- 2N FLOPs
   for(int i=1 ; i<n ; i++){
-    gtilde[i] = g[i] + gtilde[i-1]*edlitb[i-1];
+    g[i] = g[i] + g[i-1]*edlitb[i-1];
   }
   // Backward substitution -- 2N FLOPs
   for(int i=n+1 ; i>1 ; i--){
-    u[i-1] = (gtilde[i-2] + u[i])*edlitb[i-2];
+    u[i-1] = (g[i-2] + u[i])*edlitb[i-2];
   }
-  delete [] g; delete [] gtilde; delete [] edlitb;
+  delete [] g; delete [] edlitb;
   return u;
 }
 
 double error(int n, double *u, double *v){
   // find the maximum relative error
-  double maxerror = -1e4; // probably smaller than any max error
-  for(int i=1 ; i<n+1 ; i++){
+  double maxerror = log10(abs((u[1] - v[1])/v[1])); // first error
+  for(int i=2 ; i<n+1 ; i++){
     double epsilon = log10(abs((u[i] - v[i])/v[i])); // Why log10???
     //cout << epsilon << " ";
     if(epsilon >= maxerror){
--- doc/Projects/2020/Project1/code/project1.py
+++ doc/Projects/2020/Project1/code/project1.py
@@ -3,7 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys, os
 
-os.system("make reset") # delete datafiles from previous runs
+try:
+    sys.argv[3] == "-d"
+    os.system("make reset") # delete datafiles from previous runs
+except:
+    pass
 
 # Compile c++ code:
 os.system("c++ -o Project1b Project1b.cpp")
@@ -13,13 +17,27 @@ path = "P1data"
 if not os.path.isdir("./"+path):
     os.mkdir(path)
 
+# make a figure to plot data:
+fig,ax = plt.subplots(2,1)
+ax[0].set_title("Numerical solution for various $n$, along with analytical solution")
+ax[0].set_xlabel("$x$")
+ax[0].set_ylabel("$u(x)$")
+ax[1].set_title("Maximum relative error $\epsilon$ as a function of the steplength $h$")
+ax[1].set_xlabel("$\log_{10}(h)$")
+ax[1].set_ylabel("$\log_{10}(\epsilon)$")
+
+
 # Parameters
-exponents = np.linspace(1,5,5) # exponents define size of matrix
-algorithm = 'specific'
+try: # Read from command line
+    algorithm = str(sys.argv[1]) # algorithm to be used by solver
+    exponents = eval(sys.argv[2]) # exponent list to define size of matrix
+except :
+    print("Bad usage. Use %s <algorithm> <exponents>"%sys.argv[0])
+    sys.exit(1)
 
 maxerror = np.zeros(len(exponents)) # store maximum relative error
 log10h = np.zeros(len(exponents)) # store log10(h)
-Times = np.zeros(len(exponents)) # store timer values in s
+Times = np.zeros(len(exponents)) # store timer values in seconds
 
 for i in range(len(exponents)):
     exponent = exponents[i]
@@ -38,9 +56,9 @@ for i in range(len(exponents)):
             print('running project1 failed'); sys.exit(1)
 
         os.system("mv "+filename+" ./"+path+"/") # place datafile in directory
-        print("Done!")
 
 
+    print("Reading data ...")
     infile = open("./"+path+"/"+filename,'r')
 
     infile.readline() # skip first line
@@ -66,16 +84,25 @@ for i in range(len(exponents)):
     Times[i] = float(infile.readline())
     infile.close()
 
-    print("Adding data to plot")
+    print("Adding data to plot ...")
 
-    x = np.linspace(0,1,n+2)
-    plt.plot(x,u,label='$n=10^%d$'%exponent)
+    if exponent > 3:
+        x = np.linspace(0,1,1001) # No use plotting 10 000 points
+        step = int(10**(exponent-3))
+        ax[0].plot(x,u[::step],label='$n=10^%d$'%exponent)
+    else:
+        x = np.linspace(0,1,n+2)
+        ax[0].plot(x,u,label='$n=10^%d$'%exponent)
 
-plt.plot(x,v,'r--',label='analytical')
-plt.legend(loc=1)
+ax[0].plot(x,v[::step],'r--',label='analytical')
+ax[0].legend(loc=1)
+
+ax[1].plot(log10h,maxerror,'*')
 
 print("\nlog10(h) : ",log10h)
 print("maxerror : ",maxerror)
 print("time [s] :",Times)
 
+plt.tight_layout()
+plt.savefig("./"+path+"/P1_%d%d_%s.eps"%(exponents[0],exponents[-1],algorithm[0]))
 plt.show()
