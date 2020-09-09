26caf9a73dfb09193a3c6ff9b580e54b61a3e339
johashei <j.s.heines@fys.uio.no>
2020-09-09 12:04:37 +0200

    added timing functionality

--- doc/Projects/2020/Project1/code/project1.cpp
+++ doc/Projects/2020/Project1/code/project1.cpp
@@ -76,26 +76,30 @@ int main(int argc, char *argv[]){
   double maxerror = error(n,u,v_array);
   cout << " Done." << endl;
 
-  // Write results to file
-  //start = clock(); // New clock to time write to file
-  cout << "Writing results to file ..." << flush;
-  ofstream ofile;
-  ofile.open(outfilename);
-  ofile << "Solution to differential equation with "<< argv[3] << " algorithm:\n";
-  ofile << n << "\n";
-  for(int i=0; i<n+2; i++){
+  if(outfilename != "None"){// Write results to file
+    //start = clock(); // New clock to time write to file
+    cout << "Writing results to file ..." << flush;
+    ofstream ofile;
+    ofile.open(outfilename);
+    ofile << "Solution to differential equation with "<< argv[3] << " algorithm:\n";
+    ofile << n << "\n";
+    for(int i=0; i<n+2; i++){
     ofile << setw(15) << setprecision(8) << u[i] << " ";
     ofile << setw(15) << setprecision(8) << v_array[i] << "\n";
   }
-  ofile << "Max relative error and log10(h):\n";
-  ofile << setw(15) << setprecision(8) << maxerror << " ";
-  ofile << setw(15) << setprecision(8) << log10(h) << "\n";
-  ofile << "Time taken by the algorithm [ms]:\n";
-  ofile << Time << "\n";
-  ofile.close();
-  cout << " Done." << endl;
-  //finish=clock();
-  //cout << double(finish - start)/CLOCKS_PER_SEC << endl;
+    ofile << "Max relative error and log10(h):\n";
+    ofile << setw(15) << setprecision(8) << maxerror << " ";
+    ofile << setw(15) << setprecision(8) << log10(h) << "\n";
+    ofile << "Time taken by the algorithm [ms]:\n";
+    ofile << Time << "\n";
+    ofile.close();
+    cout << " Done." << endl;
+    //finish=clock();
+    //cout << double(finish - start)/CLOCKS_PER_SEC << endl;
+  }
+  else{// Display Time. Used for timing experiment { } symbols used to mark position of time value
+    cout << "Time taken by the algorithm [ms]: {" << Time << "}" << endl;
+  }
 
   delete [] u; delete [] v_array;
 
@@ -122,13 +126,13 @@ double* general_algo(int n, double h){
   // Solution, assuming boundary u(0) = u(1) = 0
   u[0] = 0;
   u[n+1] = 0;
-  // Forward substitution -- 5n FLOPs; 8 fetches; 3 writes
+  // Forward substitution -- 5n FLOPs; 8n fetches; 3n writes
   for(int i=1 ; i<n ; i++){
     double row_reduction_factor = a[i-1]/b[i-1]; // used twice so saves one FLOP
     b[i] = b[i] - c[i-1]* row_reduction_factor;
     g[i] = g[i] - g[i-1]* row_reduction_factor;
   }
-  //Backward substitution -- 3n FLOPs; 4 fetches; 1 writes
+  //Backward substitution -- 3n FLOPs; 4n fetches; 1n writes
   for(int i=n+1 ; i>1 ; i--){
     u[i-1] = (g[i-2] - c[i-2]*u[i])/b[i-2]; // -2 because they start at 0 not 1
   }
@@ -154,11 +158,11 @@ double* specific_algo(int n, double h){
   for(int i=1 ; i<n ; i++){
     edlitb[i] = double(i+1)/(i+2);
   }
-  // Forward substitution -- 2n FLOPs; 3n fetches; 1n write
+  // Forward substitution -- 2n FLOPs; 3n fetches; 1n writes
   for(int i=1 ; i<n ; i++){
     g[i] = g[i] + g[i-1]*edlitb[i-1];
   }
-  // Backward substitution -- 2n FLOPs; 3n fetches; 1n write
+  // Backward substitution -- 2n FLOPs; 3n fetches; 1n writes
   for(int i=n+1 ; i>1 ; i--){
     u[i-1] = (g[i-2] + u[i])*edlitb[i-2];
   }
