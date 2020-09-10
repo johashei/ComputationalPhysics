b3cc7f891bf7e7d7bea36e3b27b9c25b6fd9ea82
johashei <j.s.heines@fys.uio.no>
2020-09-10 11:02:42 +0200

    final code and benchmarks

--- doc/Projects/2020/Project1/code/project1.cpp
+++ doc/Projects/2020/Project1/code/project1.cpp
@@ -15,9 +15,9 @@ clock_t start, finish; // declare start and final time
 double f(double); //  right hand side function
 double v(double); //  analytical solution function
 double error(int,double*,double*); // maximum error
-double* general_algo(int,double); // general tridiagonal solver
-double* specific_algo(int,double); // 2nd order DE solver
-double* LU_algo(int,double); // LU decomp and solver from lib.h
+double* general_algo(int,double,clock_t&,clock_t&); // general tridiagonal solver
+double* specific_algo(int,double,clock_t&,clock_t&); // 2nd order DE solver
+double* LU_algo(int,double,clock_t&,clock_t&); // LU decomp and solver from lib.h
 
 
 int main(int argc, char *argv[]){
@@ -31,35 +31,28 @@ int main(int argc, char *argv[]){
   }
   outfilename = argv[1];
   n = atoi(argv[2]);
-  double h = 1.0/(n+1); // stepsize x_0 = 0 and x_n+1 = 1
+  double h = 1.0/(n+1); // stepsize; x_0 = 0 and x_n+1 = 1
 
 
   // Run and time algorithm:
   double* u;
   if(strcmp(argv[3], "general") == 0){
     cout << "Solving with general algo ..." << flush;
-    start = clock(); // start timer
-    u = general_algo(n,h);
-    cout << " Done." << endl;
+    u = general_algo(n,h,start,finish);
   }
   else if(strcmp(argv[3], "specific") == 0){
     cout << "Solving with specific algo ..." << flush;
-    start = clock(); // start timer
-    u = specific_algo(n,h);
-    cout << " Done." << endl;
+    u = specific_algo(n,h,start,finish);
   }
   else if(strcmp(argv[3], "LU") == 0){
     cout << "Solving with LU decomposition algo ..." << flush;
-    start = clock(); // start timer
-    u = LU_algo(n,h);
-    cout << " Done." << endl;
+    u = LU_algo(n,h,start,finish);
   }
   else{
     cout << "Bad usage. <algorithm> should be 'general', 'specific' or 'LU'." << endl;
     exit(1);
   }
-  finish = clock(); // stop timer
-  //cout << finish << " "<< start << " " << CLOCKS_PER_SEC << endl;
+  cout << " Done." << endl;
   double Time = (double(finish - start)/CLOCKS_PER_SEC)*1e3; // time in ms
 
 
@@ -97,7 +90,7 @@ int main(int argc, char *argv[]){
     //finish=clock();
     //cout << double(finish - start)/CLOCKS_PER_SEC << endl;
   }
-  else{// Display Time. Used for timing experiment { } symbols used to mark position of time value
+  else{// Display Time. Used for timing experiment. { } marks position of time value
     cout << "Time taken by the algorithm [ms]: {" << Time << "}" << endl;
   }
 
@@ -106,7 +99,7 @@ int main(int argc, char *argv[]){
   return 0;
 }
 
-double* general_algo(int n, double h){
+double* general_algo(int n, double h, clock_t& start, clock_t& finish){
   // Creating arrays:
   double *a = new double [n-1];
   double *b = new double [n];
@@ -126,6 +119,7 @@ double* general_algo(int n, double h){
   // Solution, assuming boundary u(0) = u(1) = 0
   u[0] = 0;
   u[n+1] = 0;
+  start = clock(); // Start timer
   // Forward substitution -- 5n FLOPs; 8n fetches; 3n writes
   for(int i=1 ; i<n ; i++){
     double row_reduction_factor = a[i-1]/b[i-1]; // used twice so saves one FLOP
@@ -136,11 +130,12 @@ double* general_algo(int n, double h){
   for(int i=n+1 ; i>1 ; i--){
     u[i-1] = (g[i-2] - c[i-2]*u[i])/b[i-2]; // -2 because they start at 0 not 1
   }
+  finish = clock(); // Stop timer
   delete [] a; delete [] b; delete [] c; delete [] g;
   return u;
 }
 
-double* specific_algo(int n, double h){
+double* specific_algo(int n, double h,clock_t& start, clock_t& finish){
   // Special case where a_i = c_i = -1 and b_i = 2 for all i
   // Creating arrays:
   double *g = new double [n];
@@ -158,6 +153,7 @@ double* specific_algo(int n, double h){
   for(int i=1 ; i<n ; i++){
     edlitb[i] = double(i+1)/(i+2);
   }
+  start = clock(); // Start timer
   // Forward substitution -- 2n FLOPs; 3n fetches; 1n writes
   for(int i=1 ; i<n ; i++){
     g[i] = g[i] + g[i-1]*edlitb[i-1];
@@ -166,11 +162,12 @@ double* specific_algo(int n, double h){
   for(int i=n+1 ; i>1 ; i--){
     u[i-1] = (g[i-2] + u[i])*edlitb[i-2];
   }
+  finish = clock(); // Stop timer
   delete [] g; delete [] edlitb;
   return u;
 }
 
-double* LU_algo(int n, double h){
+double* LU_algo(int n, double h, clock_t& start, clock_t& finish){
   // Creating arrays:
   double **A;
   double *g = new double[n];
@@ -197,9 +194,11 @@ double* LU_algo(int n, double h){
   for(int i=0 ; i<n; i++){
     g[i] = h*h*f(i*h);
   }
+  start = clock(); // Start timer
   // Call functions from lib
   ludcmp(A,n,indx,&d); // LU decomposition
   lubksb(A,n,indx,g); // Solves Au = g, stores u in g
+  finish = clock(); // Stop timer
   // Put solution in array u
   u[0] = 0;
   u[n+2] = 0;
