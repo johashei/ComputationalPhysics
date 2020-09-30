a30a6c42611829ce9770179c7b9e9118347f9b33
johashei <j.s.heines@fys.uio.no>
2020-09-30 20:02:30 +0200

    un# compiling

--- doc/Projects/2020/Project2/code/project2.py
+++ doc/Projects/2020/Project2/code/project2.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import sys, os, subprocess, glob
 
 print("Compiling...")
-#subprocess.run(['make','main','-C','cpp_code'],check=True)
-#subprocess.run(['make','test','-C','cpp_code'],check=True)
-#subprocess.run(['mv']+glob.glob('cpp_code/*.out')+['./'],check=True)
+subprocess.run(['make','main','-C','cpp_code'],check=True)
+subprocess.run(['make','test','-C','cpp_code'],check=True)
+subprocess.run(['mv']+glob.glob('cpp_code/*.out')+['./'],check=True)
 
 
 rho_0 = float(input("input rho_0 : "))
@@ -18,7 +18,7 @@ if (algo == "Lanczos"):
 
 
 print("Executing...")
-#subprocess.run(['./test.out'],check=True) # check raises error if tests fail
+subprocess.run(['./test.out'],check=True) # check raises error if tests fail
 subprocess.run(['./main.out',str(rho_0),str(rho_N),str(N),algo,str(M)],check=True)
 
 
