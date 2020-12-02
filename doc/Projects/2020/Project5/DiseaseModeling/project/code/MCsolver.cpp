db9aa1ab8d7061a0c547656c2d81af83c46c849f
johashei <j.s.heines@fys.uio.no>
2020-12-02 16:44:07 +0100

    included vital dynamics

--- doc/Projects/2020/Project5/DiseaseModeling/project/code/MCsolver.cpp
+++ doc/Projects/2020/Project5/DiseaseModeling/project/code/MCsolver.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+
 int main(int argc, const char* argv[]){
   double a = atof(argv[1]);
   double b = atof(argv[2]);
@@ -15,10 +16,15 @@ int main(int argc, const char* argv[]){
   int I = atoi(argv[5]);
   int R = atoi(argv[6]);
   int MCs = atoi(argv[7]);
+  double d = atof(argv[8]);
+  double dI = atof(argv[9]);
+  double e = atof(argv[10]);
 
   int N = S+I+R;
   double t = 0;
-  double dt = min(4/(a*N), min(1/(b*N), 1/(c*N)));
+
+  double minTs[] = {4/(a*N), 1/(b*N), 1/(c*N), 1/(e*N), 1/(d*N)};
+  double dt = *min_element(minTs,minTs+5);
 
   mt19937_64 rng;
   uniform_real_distribution<double> uniform = std::uniform_real_distribution<double>(0.0,1.0);
@@ -26,14 +32,20 @@ int main(int argc, const char* argv[]){
   ofstream outfile;
   outfile.open("bA.tsv");
 
-  bool StoI, ItoR, RtoS;
+  bool StoI, ItoR, RtoS, birth, Sdeath, Ideath, Rdeath;
   for(int cycle=0;cycle<MCs;cycle++){
     StoI = uniform(rng) < a*S*I*dt/N;
     ItoR = uniform(rng) < b*I*dt;
     RtoS = uniform(rng) < c*R*dt;
+    birth = uniform(rng) < e*N*dt;
+    Sdeath = uniform(rng) < d*S*dt;
+    Ideath = uniform(rng) < (d+dI)*I*dt;
+    Rdeath = uniform(rng) < d*R*dt;
     I += StoI; S -= StoI;
     R += ItoR; I -= ItoR;
     S += RtoS; R -= RtoS;
+    S += birth;
+    S -= Sdeath; I -= Ideath; R -= Rdeath;
     t += dt;
     outfile << t << "\t" << S << "\t" << I << "\t" << R << "\n";
   }
