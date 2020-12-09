5a8c2b537939881e4c5341733051fb96825ab01a
johashei <j.s.heines@fys.uio.no>
2020-12-09 17:05:44 +0100

    class implementation of SIRS

--- doc/Projects/2020/Project5/DiseaseModeling/project/code/SIRS.cpp
+++ doc/Projects/2020/Project5/DiseaseModeling/project/code/SIRS.cpp
@@ -1,1 +1,53 @@
+#include "SIRS.hpp"
 
+void SIRS::Solve_MC(double final_time, int filestep, string outfilename){
+  int N = m_state[0] + m_state[1] + m_state[2];
+  double minTs[] = {
+    4/((m_parameters["a/N"]+m_parameters["A"])*N*N),
+    1/(m_parameters["b"]*N),
+    1/(m_parameters["c"]*N)
+  };
+  for(auto& x: m_parameters){
+    cout << x.first << " : " << x.second << endl;
+  }
+  dt = *min_element(minTs,minTs+3);
+  cout << dt << endl;
+  int MCs = int(final_time/dt);
+  cout << MCs << endl;
+
+  outfile.open(outfilename);
+  for(int cycle=0;cycle<MCs;cycle++){
+    SeasonalSIRS();
+    Vital_dynamics();
+    //Vaccination();
+    m_time += dt;
+    outfile << m_time << "\t" << m_state[0] << "\t" << m_state[1] << "\t" << m_state[2] << "\n";
+  }
+  outfile.close();
+}
+
+void SIRS::SeasonalSIRS(){
+  /* The basic SIRS model, but with periodic transmission parameter. */
+  bool StoI = uniform(rng) < (m_parameters["A"] * cos(m_parameters["w"]*m_time) + m_parameters["a/N"])*m_state[0]*m_state[1]*dt;
+  bool ItoR = uniform(rng) < m_parameters["b"]*m_state[1]*dt;
+  bool RtoS = uniform(rng) < m_parameters["c"]*m_state[2]*dt;
+  m_state[0] -= StoI; m_state[1] += StoI;
+  m_state[1] -= ItoR; m_state[2] += ItoR;
+  m_state[2] -= RtoS; m_state[0] += RtoS;
+}
+
+void SIRS::Vital_dynamics(){
+  m_state[1] += uniform(rng) < m_parameters["e"]*dt; // births
+  m_state[2] -= uniform(rng) < m_parameters["dI"]*m_state[1]*dt; // deaths from disease
+  for(int i=1;i<4;i++){
+    m_state[i] -= m_parameters["d"]*m_state[i]*dt; // deaths from other causes
+  }
+}
+
+void SIRS::Vaccination(){
+  cout << "vacc" << endl;
+  bool StoR = uniform(rng) < rVacc(m_time)*dt;
+  cout << StoR << endl;
+  m_state[0] -= StoR;
+  m_state[2] += StoR;
+}
--- doc/Projects/2020/Project5/DiseaseModeling/project/code/SIRS.hpp
+++ doc/Projects/2020/Project5/DiseaseModeling/project/code/SIRS.hpp
@@ -1,10 +1,60 @@
+#include <iostream>
 #include <fstream>
-#include "DEsolver.hpp"
+#include <random>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <unordered_map>
+/* Storing parameters in a map allows for more readable code, and ability to
+include or not any number of effects*/
+
+using namespace std;
 
 class SIRS{
+/* To add an effect to the model */
+
 private:
-  double m_a,m_b,m_c,m_N;
+  mt19937_64 rng;
+  uniform_real_distribution<double> uniform = std::uniform_real_distribution<double>(0.0,1.0);
+  ofstream outfile;
+
+  //vector<double> m_parameters = std::vector<double>(4+3+2); // A dictionary would have been so much more readable
+  double dt;
+  double m_time;
+  vector<int> m_state = std::vector<int>(3); // State of the system, as {S,I,R}
+  void SeasonalSIRS();
+  // Improvements on the basic SIRS modeL:
+  void Vital_dynamics();
+  void Vaccination();
+  double (*rVacc)(double time);
 public:
-  Solve_DE()
-  Solve_MC()
+  unordered_map<string,double> m_parameters;
+  // Variable names starting with r + [capital letter] represent rates
+  SIRS(double rStoI, double rItoR, double rRtoS);
+  void Set_state(int S, int I, int R){m_state[0] = S; m_state[1]=I; m_state[2]=R;}
+  void Solve_DE();
+  void Solve_MC(double final_time, int filestep, string outfilename);
+  vector<int> Get_state(){return m_state;}
+  double Get_time(){return m_time;}
+  // Improvements on the basic SIRS modeL: parameter inputs
+  void Vital_dynamics(double rBirth, double rDeath, double rIDeath);
+  void Seasonal_variation(double amplitude, double omega);
+  void Vaccination(double (*rate)(double time)){rVacc = rate;}
 };
+
+inline SIRS::SIRS(double rStoI, double rItoR, double rRtoS){
+  m_parameters["a/N"]=rStoI;
+  m_parameters["b"]=rItoR;
+  m_parameters["c"]=rRtoS;
+}
+
+inline void SIRS::Vital_dynamics(double rBirth, double rDeath, double rIDeath){
+  m_parameters["e"]=rBirth;
+  m_parameters["d"]=rDeath;
+  m_parameters["dI"]=rIDeath;
+}
+
+inline void SIRS::Seasonal_variation(double amplitude, double omega){
+  m_parameters["A"]=amplitude;
+  m_parameters["w"]=omega;
+}
