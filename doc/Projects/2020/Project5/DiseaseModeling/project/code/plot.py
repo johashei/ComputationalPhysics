fbbf4dbf5bfaad94613deffaa9960c0ab0d0f6a6
johashei <j.s.heines@fys.uio.no>
2020-12-18 10:58:26 +0100

    Now only plots the result from SIRS.cpp

--- doc/Projects/2020/Project5/DiseaseModeling/project/code/plot.py
+++ doc/Projects/2020/Project5/DiseaseModeling/project/code/plot.py
@@ -6,22 +6,6 @@ import SIRS
 infilename = sys.argv[1];
 plotstep = int(sys.argv[2]);
 
-# ODE calculation
-# Setting Variables:
-S_0 = 300 ; I_0 = 100 ; R_0 = 0
-dt = 0.001 ; t_i = 0 ; t_f = 30
-b = 2 ; beta = 0.01 ; c = 0.5
-
-# Making plots:
-plt.figure('Simulation of the spreading of a disease by the SIRS model')
-
-plt.subplot()
-S, I, R, t = SIRS.SIRSSolver(S_0,I_0,R_0, beta,b,c, dt,t_i,t_f)
-SIRS.visualizer(S,I,R, t, t_unit = 'days')
-plt.title("$\\beta=${beta}".format(beta=beta))
-
-plt.subplots_adjust(left=0.12, bottom=0.10, right=0.99, top=0.95, hspace=0.40)
-
 
 t = []
 SIR = []
@@ -38,20 +22,6 @@ SIR = np.array(SIR)
 plt.step(t[::plotstep],SIR[::plotstep,:],'-')
 #plt.step(t[::plotstep],N-np.sum(SIR,1),'-')
 
-#x = np.linspace(np.log(0.001),1,100)
-#plt.plot(x - np.log(0.001),np.exp(x),':r')
-"""
-plt.show(block=False)
-
-Equilibration_time = float(input("Input equilibration time : "))
-eqidx = np.where(t>Equilibration_time)[0][0]
-print(eqidx)
-
-MeanSIR = np.sum(SIR[eqidx:,:],0) / len(t[eqidx:])
-
-print(MeanSIR)
-"""
 
 
-
-plt.show(block=True)
+plt.show()
